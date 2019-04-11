7351df7668ba01e09b5504c2e56c242b373aa8cc
Felipe Sanchez <vegafelipe015@gmail.com>
2019-04-10 20:55:37 -0500

    Los algoritmos de ordenamiento clasico fueron implementados

--- src/model/Airport.java
+++ src/model/Airport.java
@@ -23,7 +23,56 @@ public class Airport {
 	
 //________________________________________________________________________________________
 	
+	public void sortByDate() { //Selection Sorting
+		for (int I = 0; I < flights.length-1; I++) {
+			String date = flights[I].getDate();
+			int minPosition = I;
+			
+			for (int J = I+1; J < flights.length; J++) {
+				String currentDate = flights[J].getDate();
+				if(currentDate.compareTo(date) < 0) {
+					date = currentDate;
+					minPosition = J;
+				}
+			}
+			
+			Flight temp = flights[minPosition];
+			flights[minPosition] = flights[I];
+			flights[I] = temp;
+		}
+	}
 	
+//____________________________________________________________________________________________
+	
+	public void sortByHour() { //Insertion sorting
+		
+		for (int I = 1; I < flights.length; I++) {
+			for (int J = I; J > 0 && flights[J-1].getHour().compareTo(flights[J].getHour()) == 1; J--) {
+				Flight temp = flights[J];
+				flights[J] = flights[J-1];
+				flights[J-1] = temp;
+			}
+		}
+		
+	}
+//_________________________________________________________________________________________
+	
+	public void sortByGate() {
+		
+		for(int i = 0; i < flights.length - 1; i++)
+        {
+            for(int j = 0; j < flights.length - 1; j++)
+            {
+                if (flights[j].getGate() > flights[j + 1].getGate())
+                {
+                    Flight temp = flights[j+1];
+                    flights[j+1] = flights[j];
+                    flights[j] = temp;
+                }
+            }
+        }      
+		
+	}
 	
 //_________________________________________________________________________________________
 	
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -13,7 +13,11 @@ public class Main extends Application{
 	public static void main(String[] args) {
 		Airport a = new Airport(10);
 		System.out.println(a.report());
-		launch(args);
+		a.sortByGate();
+		//a.sortByHour();
+		//a.sortByDate();
+		System.out.println(a.report());
+		//launch(args);
 		
 	}
 	
